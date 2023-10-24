3597b3a8c4874f873fe25f7ab21758f8b85ab5c2
Bekalu <bekpostgrad@gmail.com>
2023-10-24 10:22:20 -0500

    update configs

--- textIr/urls.py
+++ textIr/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .search.views import search
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
+import sys
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/', search, name='search'),
+    # path('', include('textIr.search.urls')),
 ]
