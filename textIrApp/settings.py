3c50fd2d839588069ded79c8fe59367d6256d2fb
Bekalu <bekpostgrad@gmail.com>
2023-11-01 23:47:43 -0500

    installed new app component tdm

--- textIrApp/settings.py
+++ textIrApp/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'searchFunc.apps.SearchfuncConfig'
+    'searchFunc.apps.SearchfuncConfig',
+    'tdm.apps.TdmConfig'
 ]
 
 MIDDLEWARE = [
