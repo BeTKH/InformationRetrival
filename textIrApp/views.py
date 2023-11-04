e8107ec007a19f631f762c10f19eba210e61140a
Bekalu <bekpostgrad@gmail.com>
2023-11-04 15:57:31 -0500

    update view function

--- textIrApp/views.py
+++ textIrApp/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from tdm.models import Documents
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
 
-from .customLibraries import cleanQuery, query_documents
+from .customLibraries import cleanQuery, query_documents, orderByRelevance
 from .createTDM import tdm, allTerms
 
 
@@ -21,12 +22,18 @@ def getQueryText(requestIndex):
         print(queryText_clean)
 
         # execute the query
-        queryResult = query_documents(tdm, allTerms, queryText_clean)
+        documentScoreArray = query_documents(tdm, allTerms, queryText_clean)
 
-        print(queryResult)
+        fileNames_score = orderByRelevance(documentScoreArray)
+
+        print(fileNames_score)
+
+        # get all document attributes from the database
+        dbDocumentFilelds = Documents.objects.all
+
         # return queryText
 
-        return HttpResponse("Received a POST request")
-
+        return render(requestIndex, 'tdmTempltes/queryResults_top.html', {'fileNames_score_': fileNames_score, 'dbDocumentFilelds_': dbDocumentFilelds})
+        # return render(requestIndex, 'tdmTempltes/index.html')
     else:
         return render(requestIndex, 'tdmTempltes/index.html')
